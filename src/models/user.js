6495e46f133ca6366ae0ec4a7535435b06894572
Jonathan Mukwala <jonathanmukwala@gmail.com>
2024-07-18 12:53:21 +0200

    Résolution des conflits

---
+++ src/models/user.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const userSchema = new mongoose.Schema({
+    email: {
+     type: String,
+     required: true,
+     unique: true,
+     lowercase: true,
+     trim: true,
+     validate(v) {
+         if (!validator.isEmail(v)) throw new Error('Email non valide !');
+     }
+    },
+    password: {
+     type: String, 
+     required: true,
+    },
+    authTokens: [{
+        authToken: {
+            type: String,
+            required: true
+        }
+    }]
+ });
+
+ userSchema.methods.toJSON = function() {
+    const user = this.toObject();
+
+    delete user.password;
+    delete user.authToken;
+
+    return user;
+ }
+
+ userSchema.methods.generateAuthTokenAndSaveUser = async function() {
+    const authToken = jwt.sign({_id: this._id.toString() }, 'foo');
+    this.authTokens.push({ authToken });
+    await this.save();
+    return authToken;
+ }
+
+ userSchema.statics.findUser = async(email, password) => {
+    const user = await User.findOne({ email });
+    if (!user) throw new Error('Erreur, pas possible de se connecter!');
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) throw new Error('Erreur, pas possible de se connecter');
+    return user;
+ }
+
+ userSchema.pre('save', async function() {
+    if (this.isModified('password')) this.password = await bcrypt.hash(this.password, 8);
+ });
+
+const User = mongoose.model('User', userSchema);
+
+ module.exports = User;
---
+++ src/routes/user.js
@@ -0,0 +1,95 @@
+const express = require ('express');
+const User = require ('../models/user');
+const authentification = require ('../middlewares/authentification');
+const router = new express.Router();
+
+
+router.post('/users/login',  async (req, res) => {
+    try {
+        const user = await User.findUser(req.body.email, req.body.password);
+        const authToken = await user.generateAuthTokenAndSaveUser();
+        res.send({user, authToken});
+    } catch(e) {
+        res.status(400).send();
+    }
+});
+
+router.post('/users/logout', authentification, async (req, res) => {
+    try {
+       req.user.authTokens = req.user.authToken.filter((authToken) => {
+        return authToken.authToken !== req.authToken;
+       });
+
+       await req.user.save();
+       res.end();
+    } catch(e) {
+        res.status(500).send()
+    }
+});
+
+router.post('/users/logout/all', authentification, async (req, res) => {
+    try {
+       req.user.authTokens = [];
+       await req.user.save();
+       res.send();
+    } catch(e) {
+        res.status(500).send();
+    }
+});
+
+router.get('/users', authentification, async (req, res, next) => {
+    try {
+        const users = await User.find({});
+        res.send(users);
+    } catch(e) {
+        res.status(500).send(e);
+    }
+});
+
+router.get('/users/me', authentification, async (req, res, next) => {
+    res.send(req.user);
+});
+
+// router.get('/users/:id', async (req, res, next) => {
+//     const userId = req.params.id;
+
+//     try {
+//         const user = await User.findById(userId);
+//         if (!user) return res.status(404).send('User not found');
+//         res.send(user);
+//     } catch(e) {
+//         res.status(500).send(e);
+//     }
+// });
+
+router.patch('/users/:id', async (req, res, next) => {
+    const updatedInfo = Object.keys(req.body);
+    const userId = req.params.id;
+
+    try {
+        const user = await User.findById(userId);
+        updatedInfo.forEach(update => user[update] = req.body[update]);
+        await user.save();
+
+        if (!user) return res.status(404).send('User not found');
+        res.send(user);
+    } catch(e) {
+        res.status(500).send(e);
+    }
+});
+
+router.delete('/users/:id', async (req, res, next) => {
+    const userId = req.params.id;
+
+    try {
+        const user = await User.findByIdAndDelete(userId);
+
+        if (!user) return res.status(404).send('User not found');
+        res.send(user);
+    } catch(e) {
+        res.status(500).send(e);
+    }
+});
+
+
+module.exports = router;
